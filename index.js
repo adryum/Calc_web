b99a7ce0c5c8d331799f99a21ee385114c2c1751
adrians <adiskir@gmail.com>
2024-11-13 22:31:12 +0200

    added history saving function and <dot> limitations

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let history = [];
 let calcScreen = document.getElementById("calc-screen");
+let historyElement = document.getElementById("history-data");
+let history = localStorage.getItem("history") === null ? [] : JSON.parse(localStorage.getItem("history"));
+let isCommaPlaced = false;
+
+updateHistory();
 // Button functionality
 document.querySelectorAll(".button").forEach(element => {
     if ( // Skips over complexer buttons
+        element.textContent === "." ||
         element.textContent === "AC" ||
         element.textContent === "="
     ) {
@@ -11,16 +16,36 @@ document.querySelectorAll(".button").forEach(element => {
     
     element.addEventListener('click', (e) => {
         calcScreen.innerHTML += e.target.textContent;
+
+        // Allows another comma to be placed after these operations
+        if (
+            e.target.textContent === "*" ||
+            e.target.textContent === "/" ||
+            e.target.textContent === "+" ||
+            e.target.textContent === "-"
+        ) {
+            isCommaPlaced = false;
+        }
     });
 });
+document.getElementById("dot").addEventListener("click", (e) => {
+    if (!isCommaPlaced) { // Allows only one comma for one number
+        calcScreen.innerHTML += e.target.textContent;
+        isCommaPlaced = true;
+    }
+});
 document.getElementById("AC").addEventListener("click", (e) => {
-   calcScreen.innerHTML = ""; 
+   calcScreen.innerHTML = "";
+    // Allows another comma to be placed
+    isCommaPlaced = false;
 });
 document.getElementById("equals").addEventListener("click", (e) => {
     // Calculate input:
     let result = calculate();
     // Saves input to history
     history.push(calcScreen.innerHTML + " = " + result);
+
+    updateHistory();
     
     // Shows result on screen
     calcScreen.innerHTML = result;
@@ -30,4 +55,30 @@ function calculate() {
     return eval(calcScreen.innerHTML);
 }
 
-function clearHistory() {}
+function clearHistory() {
+    history = [];
+    updateHistory();
+}
+
+function updateHistory() {
+    // Clears element html
+    historyElement.innerHTML = "";
+    // Adds actual html data
+    history.forEach(item => {
+        historyElement.insertAdjacentHTML('afterbegin', `
+            <div class="history-entry">
+                ${item}
+                <button class="delete-history-entry-button" type="button" onclick="clearHistoryEntry(${history.indexOf(item)})">Delete</button>
+            </div>
+       `);
+    });
+    console.log(history);
+    localStorage.setItem("history", JSON.stringify(history));
+}
+
+function clearHistoryEntry(id) {
+    // Deletes specified history entry
+    console.log(id);
+    history.splice(id, 1);
+    updateHistory();
+}
